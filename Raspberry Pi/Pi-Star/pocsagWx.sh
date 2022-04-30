f75989e5fbcfb9d899eba106d20ed680752f4c77
John H <7028086+techfixpros@users.noreply.github.com>
2022-04-30 02:23:42 -0500

    Update pocsagWx.sh

--- Raspberry Pi/Pi-Star/pocsagWx.sh
+++ Raspberry Pi/Pi-Star/pocsagWx.sh
@@ -1,45 +1,35 @@
 #!/bin/bash
 #
-# wx2pocsag.sh
+# pocsagWx.sh
+#
+# Pull Wx data from MQTT broker, send to POCSAG via RemoteCommand
 #
 # John/KI5NYZ 2022
 #
 # Configuration
 
-call="KI5NYZ"
-grid="DM82ww"
+call="N0CALL"
+grid="urGrid"
 time=$(date +"%H:%M")
-server="altair.lan"
-prefix="weewx"
-ric="1080"
-pause=0
+server="urServer"
+prefix="urMQTTprefix"
+ric="urRIC"
 
 # Get MQTT Wx Data
 outT=$(mosquitto_sub -h $server -C 1 -t $prefix/outTemp_F | awk -F '.' '{print $1 "." substr ($2, 0, 2)}')
-sleep $pause
 
 windS=$(mosquitto_sub -h $server -C 1 -t $prefix/windSpeed_mph | awk -F '.' '{print $1 "." substr ($2, 0, 2)}')
-sleep $pause
 
 windD=$(mosquitto_sub -h $server -C 1 -t $prefix/windDir | awk -F '.' '{print $1}')
-sleep $pause
 
 humid=$(mosquitto_sub -h $server -C 1 -t $prefix/outHumidity | awk -F '.' '{print $1}')
-sleep $pause
 
 baro=$(mosquitto_sub -h $server -C 1 -t $prefix/barometer_inHg | awk -F '.' '{print $1 "." substr ($2, 0, 3)}')
-sleep $pause
 
 rainR=$(mosquitto_sub -h $server -C 1 -t $prefix/rainRate_inch_per_hour | awk -F '.' '{print $1 "." substr ($2, 0, 2)}')
-sleep $pause
 
 #rainT=$(mosquitto_sub -h $server -C 1 -t $prefix/rain_in | awk -F '.' '{print $1 "." substr ($2, 0, 2)}')
-#sleep $pause
 
 lux=$(mosquitto_sub -h $server -C 1 -t $prefix/radiation_Wpm2 | awk -F '.' '{print $1 "." substr ($2, 0, 2)}')
-sleep $pause
-
-# Debug to CLI
-#echo "$time $call/$grid: ${outT}F w: ${windS}mi/h=${windD}deg h: $humid% inHg: $baro r: ${rainR}in/h l: ${lux}Wpm2"
 
 sudo RemoteCommand 7642 page $ric "$call/$grid: ${outT}F w: ${windS}mi/h=${windD}deg h: $humid% inHg: $baro r: ${rainR}in/h l: ${lux}Wpm2"
